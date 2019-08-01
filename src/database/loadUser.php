57362c8c973520b850564aefd05fc0bd9c67ea08
Andy Tan <andytan0727@gmail.com>
2019-08-02 05:09:13 +0800

    fix: check if cookie exists before indexing

--- src/database/loadUser.php
+++ src/database/loadUser.php
@@ -2,14 +2,13 @@
 use ReallySimpleJWT\Token;
 use NotesGalleryApp\Database\MySqlDB;
 use NotesGalleryApp\Repositories\UserRepository;
-
-$token = $_COOKIE['TOKEN'];
+use function NotesGalleryLib\helpers\saveUserToSession;
 
 // init user if token exists in cookie
-if ($token) {
+if (isset($_COOKIE['TOKEN'])) {
+    $token = $_COOKIE['TOKEN'];
     $payload = Token::getPayload($token, $_ENV['TOKEN_KEY']);
     $userRepo = new UserRepository(new MySqlDB());
     $user = $userRepo->findOne($payload['user_id']);
-    $_SESSION['CURRENT_USER_ID'] = $user->id;
-    $_SESSION['CURRENT_USER_NAME'] = $user->username;
+    saveUserToSession($user);
 }
